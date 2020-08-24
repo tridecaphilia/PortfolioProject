10ec8e296df7a2c1a8978a98126f60156a75b77e
Monica <monicastephens2003@gmail.com>
2020-08-24 03:53:51 -0700

    Portfolio Project_Bootstrap

---
+++ Desktop/NucampFolder/2-Bootstrap/project/assets/js/jquery.js
@@ -0,0 +1,37 @@
+// Simple-Slider
+$(function(){
+
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    });
+    
+});
+
+//Card Carousel
+
+$('#carouselExample').on('slide.bs.carousel', function (e) {
+
+    var $e = $(e.relatedTarget);
+    var idx = $e.index();
+    var itemsPerSlide = 4;
+    var totalItems = $('.carousel-item').length;
+    
+    if (idx >= totalItems-(itemsPerSlide-1)) {
+        var it = itemsPerSlide - (totalItems - idx);
+        for (var i=0; i<it; i++) {
+            // append slides to end
+            if (e.direction=="left") {
+                $('.carousel-item').eq(i).appendTo('.carousel-inner');
+            }
+            else {
+                $('.carousel-item').eq(0).appendTo('.carousel-inner');
+            }
+        }
+    }
+});
+
+//4 col small slider
